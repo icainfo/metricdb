98979446828eb2655f5639b466dd24daab83994c
jhaines301 <104173230+jhaines301@users.noreply.github.com>
2025-02-14 13:17:00 -0500

    Added AverageResponseTime component

--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@ import TicketsByReportMethod from "./components/TicketsByReportMethod";
 import TicketsByServiceType from "./components/TicketsByServiceType";
 import TicketsByLocation from "./components/TicketsByLocation";
 import TicketsByDepartment from "./components/TicketsByDepartment";
+import AverageResponseTime from "./components/AverageResponseTime";
 
 export default function Home() {
   return (
     <div>
       <h1>HelpScout Metrics Dashboard</h1>
       <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "20px" }}>
+        <AverageResponseTime />
         <TicketsByCategory />
         <TicketsByReportMethod />
         <TicketsByServiceType />
